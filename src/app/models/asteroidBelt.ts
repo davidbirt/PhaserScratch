7cfae1039dd5d6fdcc1f1b45678b216f4f948838
dbz <dbirt3168@outlook.com>
2017-10-10 11:16:08 -0400

    enhance: level concept build out a little more to include various asteroid sizes

--- src/app/models/asteroidBelt.ts
+++ src/app/models/asteroidBelt.ts
@@ -1,7 +1,7 @@
 /// <reference path="../services/phaser.d.ts" />
 import { GameObject } from './gameEntity';
 import { GameSettings } from './gameSettings'
-import { GameLevel } from './level'
+import { GameRule } from './level'
 
 export class AsteroidBelt extends GameObject{
     constructor(game : Phaser.Game ){ 
@@ -15,28 +15,32 @@ export class AsteroidBelt extends GameObject{
     list:Phaser.Group;
     settings : GameSettings;
 
-    buildAsteroids(level:GameLevel) {
+    buildAsteroids(level: { Rules: GameRule[] }) {
         console.log(level);
-        for (var i=0; i < level.count; i++ ) {
-            var side = Math.round(Math.random());
-            var x;
-            var y;
-            
-            if (side) {
-                x = Math.round(Math.random()) * this.game.width;
-                y = Math.random() * this.game.height;
-            } else {
-                x = Math.random() * this.game.width;
-                y = Math.round(Math.random()) * this.game.height;
+        for (var key in level.Rules) {
+            var rule = level.Rules[key];
+
+            for (var i = 0; i < rule.count; i++) {
+                var side = Math.round(Math.random());
+                var x;
+                var y;
+
+                if (side) {
+                    x = Math.round(Math.random()) * this.game.width;
+                    y = Math.random() * this.game.height;
+                } else {
+                    x = Math.random() * this.game.width;
+                    y = Math.round(Math.random()) * this.game.height;
+                }
+
+                var asteroid = this.list.create(x, y, rule.asteroid);
+                asteroid.anchor.set(0.5, 0.5);
+                asteroid.body.angularVelocity = this.game.rnd.integerInRange(0, 200);
+
+                var randomAngle = Phaser.Math.degToRad(this.game.rnd.angle());
+                var randomVelocity = this.game.rnd.integerInRange(50, 150);
+                this.game.physics.arcade.velocityFromRotation(randomAngle, randomVelocity, asteroid.body.velocity);
             }
-            
-            var asteroid = this.list.create(x, y, level.asteroid);
-            asteroid.anchor.set(0.5, 0.5);
-            asteroid.body.angularVelocity = this.game.rnd.integerInRange(0, 200);
-     
-            var randomAngle = Phaser.Math.degToRad(this.game.rnd.angle());
-            var randomVelocity = this.game.rnd.integerInRange(50, 150);
-            this.game.physics.arcade.velocityFromRotation(randomAngle, randomVelocity, asteroid.body.velocity);
         }
     }
 }
